5275d96e8dfadcfd4e7a64a2dde90c6547074ea1
Šárka Mráčková <sarka7091@gmail.com>
2022-04-22 20:37:24 +0200

    uprava stylu a js

--- hra.js
+++ hra.js
@@ -1,15 +1,19 @@
-function letsMagicHappens() {
-  const buttons = document.querySelectorAll('button');
-  const ikonaCircle = 'circle';
-  const ikonaCross = 'cross';
-  const move = document.querySelector('.circle');
+const buttons = document.querySelectorAll('button');
+let ikona = 'circle';
 
-  const moveCircle = (event) => {
-    event.target.classList.add('field--circle');
-  };
-
-  for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', moveCircle);
-  }
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', (event) => {
+    if (ikona === 'circle') {
+      event.target.classList.add('field--circle');
+      const ikonaHrace = document.querySelector('.circle');
+      ikonaHrace.src = 'img/cross.svg';
+      ikona = 'cross';
+    } else {
+      event.target.classList.add('field--cross');
+      const ikonaHrace = document.querySelector('.circle');
+      ikonaHrace.src = 'img/circle.svg';
+      ikona = 'circle';
+    }
+    buttons[i].disabled = true;
+  });
 }
-/*fce jejíž kód se volá až po načtení html*/
